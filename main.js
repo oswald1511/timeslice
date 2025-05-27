994096b77af4443f9b5139452fbc8e7f7040c414
oswald <omaldonado@fi.uba.ar>
2025-05-27 01:03:25 -0300

    cambio la imagen del proyecto, ahora indica en que dia de la semana estas

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: path.join(__dirname, 'src', 'assets', 'garrapatas.png'), // <-- aquí
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false, // ⚠️ Esto desactiva el aislamiento, solo si sabés lo que hacés
--- src/renderer.js
+++ src/renderer.js
@@ -4,28 +4,63 @@ const localeEs = require('dayjs/locale/es');  // Importa el idioma español
 dayjs.locale(localeEs);
 
 // Obtener la fecha actual formateada
-const fechaActual = dayjs().format('dddd, DD MMMM YYYY');
+const mesActual = dayjs().format('MMMM');
 
 // Mostrar la fecha en el párrafo
-document.getElementById('fecha').textContent = `Hoy es: ${fechaActual}`;
-const diasEnELMes = dayjs().daysInMonth();
+document.getElementById('fecha').textContent = mesActual;
 
 const tabla = document.getElementById('calendario');
 
 const diaActual = dayjs().date();
-let fila = tabla.insertRow();
 
-for (let i = 0; i < diasEnELMes; i++) {
+const  primerDiaDelMes = (dayjs().startOf('month').day()) % 7;
+
+let fila = document.createElement('div');
+fila.className = 'row';
+
+let i = 0;
+while(i + 1 < primerDiaDelMes){
+    const celda = document.createElement('div');
+    celda.className = 'col';
+    celda.innerHTML = '&nbsp;';
+    fila.appendChild(celda);
+    i++;
+}
+
+const diasEnELMes = dayjs().daysInMonth();
+
+tabla.appendChild(fila);
+
+for (let j = 0; j < diasEnELMes; j++) {
     if (i % 7 === 0){
-        fila = tabla.insertRow();
+        fila = document.createElement('div');
+        fila.className = 'row';
+        tabla.appendChild(fila);
     }
-    const celda = fila.insertCell();
-    celda.textContent = i + 1;
+    const celda = document.createElement('div');
+    celda.className = 'col';
+    celda.textContent = j + 1;
 
-    if (i + 1 === diaActual) {
+    if (j + 1 === diaActual) {
         celda.style.backgroundColor = "lightblue"; // marcar el día actual    
     }
+    fila.appendChild(celda);
+    i++;
 }
+
+// Si la última fila no tiene 7 columnas, agrega celdas vacías
+const celdasEnUltimaFila = i % 7;
+if (celdasEnUltimaFila !== 0) {
+    for (let k = celdasEnUltimaFila; k < 7; k++) {
+        const celdaVacia = document.createElement('div');
+        celdaVacia.className = 'col';
+        celdaVacia.innerHTML = '&nbsp;';
+        fila.appendChild(celdaVacia);
+    }
+}
+
+
+
 
 
 
